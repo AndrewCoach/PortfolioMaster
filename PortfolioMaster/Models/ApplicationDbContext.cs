fe0d157d882cc2bc58f8e3198c817b6bd99329f3
Andrew Sharp <kociandreas1@gmail.com>
2023-04-02 11:10:46 +0200

    Project initialization

---
+++ PortfolioMaster/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioMaster.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet properties for your entities here
+    }
+}
+
